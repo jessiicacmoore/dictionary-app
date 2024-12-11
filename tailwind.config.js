7116de0ca7ae0f43d18a0e1d2eef0409965cbcfd
Jessica Moore <jessica.cmoore1@gmail.com>
2024-12-11 14:20:20 -0500

    :wrench: Initial Tailwind theme config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 export default {
-  content: [],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          purple: '#a445ed',
+        },
+        gray: {
+          100: '#fefefe',
+          200: '#f4f4f4',
+          300: '#9ca3b0',
+          500: '#757575',
+          600: '#2d2d2d',
+          700: '#050505',
+        },
+      },
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        serif: ['Lora', ...defaultTheme.fontFamily.serif],
+        mono: ['Inconsolata', ...defaultTheme.fontFamily.mono],
+      },
+    },
   },
   plugins: [],
-}
-
+};
