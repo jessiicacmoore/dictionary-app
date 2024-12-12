18d12153dc18536351c6a3e12dbe122d89a336b0
Jessica Moore <jessica.cmoore1@gmail.com>
2024-12-12 17:15:45 -0500

    :wrench: Added @app alias

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      "@app": path.resolve(__dirname, "src/app"),
       "@features": path.resolve(__dirname, "src/features"),
       "@components": path.resolve(__dirname, "src/components"),
       "@hooks": path.resolve(__dirname, "src/hooks"),
